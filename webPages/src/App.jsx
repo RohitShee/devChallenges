e0717dcbd370865d8fbd7ea8567f08c18fbca697
RohitShee <rohitshee92@gmail.com>
2024-11-03 21:55:45 +0530

    minimal blog post card

---
+++ webPages/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import BlogCard from './components/BlogCard'
+
+
+function App() {
+
+
+  return (
+    <div className='h-screen mx-auto my-auto flex items-center justify-center pt-[72px] bg-slate-100'>
+     <BlogCard/>
+    </div>
+  )
+}
+
+export default App
